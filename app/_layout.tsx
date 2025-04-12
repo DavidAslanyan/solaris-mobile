890a6fc3db3ed9f6c73f0d65ba1e6c8e37ae3c1e
DavidAslanyan <david.aslanyan@acquerant.com>
2025-04-12 15:04:12 +0400

    Refactored store page based on dark mode and screen size

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack, useRouter } from 'expo-router';
+import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeCustomProvider } from './contexts/ThemeContext';
-import Store from './(tabs)/store';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const router = useRouter();
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -34,15 +28,14 @@ export default function RootLayout() {
   }
 
   return (
-    <Store />
-    // <QueryClientProvider client={queryClient}>
-    //   <ThemeCustomProvider>
-    //     <Stack screenOptions={{ headerShown: false }}>
-    //       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    //       <Stack.Screen name="+not-found" />
-    //     </Stack>
-    //     <StatusBar style="auto" />
-    //   </ThemeCustomProvider>
-    // </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeCustomProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeCustomProvider>
+    </QueryClientProvider>
   );
 }
