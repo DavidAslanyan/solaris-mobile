3465cb738c3a8d31a08fa93609c389cfa3a4209c
DavidAslanyan <david.aslanyan@acquerant.com>
2025-04-15 21:51:41 +0400

    Developed exit and delete user popups

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -18,6 +18,16 @@ import { useState } from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import { useAppTheme } from '../contexts/ThemeContext';
 import { router } from 'expo-router';
+import Popup from '@/components/popup';
+import FailIcon from '@/components/icons/FailIcon';
+import ButtonSecondarySmall from '@/components/buttons/button-secondary-small';
+import ButtonMainSmall from '@/components/buttons/button-main-small';
+
+
+enum PopupOption {
+  signOut = "signOut",
+  deleteAccount = "deleteAccount"
+};
 
 const Profile = () => {
   const data = {
@@ -40,6 +50,27 @@ const Profile = () => {
   const iconColor = theme == 'dark' ? Colors.white : Colors.secondary;
   const [darkMode, setDarkMode] = useState<boolean>(false);
   const [notifications, setNotifications] = useState<boolean>(false);
+  const [difficultyPopupOpen, setDifficultyPopupOpen] = useState<boolean>(false);
+  const [popup, setPopup] = useState<PopupOption | null>(null);
+
+  const handlePopupPress = () => {
+    if (popup === PopupOption.signOut) {
+      try {
+        // logout();
+        router.replace('/pages/login');
+      } catch(error) {
+        console.log("Failed to sign out", error);
+      }
+    }
+    if (popup === PopupOption.deleteAccount) {
+      try {
+        // deleteUser();
+        router.replace('/pages/register');
+      } catch(error) {
+        console.log("Failed to delete user", error);
+      }
+    }
+  }
 
   return (
     <ParallaxScrollView 
@@ -144,14 +175,30 @@ const Profile = () => {
         <OptionTab
           title='Delete Account'
           icon={<TrashIcon color={iconColor} />}
-          onPress={() => {}}
+          onPress={() => setPopup(PopupOption.deleteAccount)}
         />
         <OptionTab
           title='Sign Out'
           icon={<ExitIcon color={iconColor} />}
-          onPress={() => {}}
+          onPress={() => setPopup(PopupOption.signOut)}
         />
       </View>
+
+
+      <Popup isOpen={popup !== null} setIsOpen={() => {}}>
+        <View style={styles.popupContainer}>
+          {popup === PopupOption.deleteAccount 
+          ? <FailIcon width={120} height={120} color={Colors.red} /> 
+          : <ExitIcon color={theme === 'dark' ? Colors.white : Colors.secondary} width={80} height={80} />
+          }
+          <ThemeText style={{textAlign: 'center', paddingVertical: 10}} size='md' weight='medium'>Are you sure you want to {popup === PopupOption.deleteAccount ? " delete your account" : " sign out"}
+          ?</ThemeText>
+          <View style={styles.buttonContainer}>
+            <ButtonSecondarySmall onPress={() => setPopup(null)} title='Cancel' />
+            <ButtonMainSmall title={popup === PopupOption.deleteAccount ? "Delete My Account" : "Sign Out"} />
+          </View>
+        </View>
+      </Popup>
       
     </ParallaxScrollView>
   )
@@ -213,6 +260,15 @@ const styles = StyleSheet.create({
   },
 
   list: {
+    gap: 10,
+    paddingBottom: 10
+  },
+  popupContainer: {
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
     gap: 10
   }
 })
