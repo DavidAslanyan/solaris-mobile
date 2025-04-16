7d83af28de2aa80212a60fc88502dca28f143a6c
DavidAslanyan <david.aslanyan@acquerant.com>
2025-04-16 16:26:06 +0400

    Developed change difficulty popup

--- components/lottie-animations/lottie-robot/Robot.tsx
+++ components/lottie-animations/lottie-robot/Robot.tsx
@@ -1,12 +1,20 @@
-import Lottie from "lottie-react";
+import LottieView from 'lottie-react-native';
 import lottie from './lottie-robot.json';
 import { View } from "react-native";
 
 
-const Robot = () => {
+const Robot = ({ width = 200, height = 200 }: { width?: number, height?: number }) => {
   return (
-  <View className="w-full max-w-[20rem]">
-    <Lottie animationData={lottie} />
+    <View>
+    <LottieView 
+      source={lottie}
+      autoPlay
+      loop
+      style={{
+        width: width,
+        height: height
+      }}
+    />
   </View>
   );
 };
